2d4fa2971b3700cd70c44868cb4d99a9216ddf4a
dreadster3 <afonso.antunes@live.com.pt>
2023-06-30 22:35:45 +0100

    Rename install-folder to output-folder

--- lua/tasks/module/conan.lua
+++ lua/tasks/module/conan.lua
@@ -2,33 +2,25 @@ local path = require("plenary.path")
 local os = require('ffi').os:lower()
 
 local function conan_install(module_config, _)
-	local build_folder = path:new(vim.loop.cwd(), "build", os .. "-" .. module_config.build_type)
+    local build_folder = path:new(vim.loop.cwd(), "build",
+                                  os .. "-" .. module_config.build_type)
 
-	if not build_folder:exists() then
-		build_folder:mkdir({parents = true})
-	end
+    if not build_folder:exists() then build_folder:mkdir({parents = true}) end
 
-	local args = {"install", "--install-folder", build_folder.filename }
+    local args = {"install", "--output-folder", build_folder.filename}
 
-	table.insert(args, "./")
-	return {
-		cmd = module_config.cmd,
-		args = args,
-		only_on_error = module_config.only_on_error
-	}
+    table.insert(args, "./")
+    return {
+        cmd = module_config.cmd,
+        args = args,
+        only_on_error = module_config.only_on_error
+    }
 end
 
 local conan = {
-	params = {
-		'cmd',
-		build_type = {"debug", "release"},
-	},
-	condition = function ()
-		return path:new('conanfile.txt'):exists()
-	end,
-	tasks = {
-		install = conan_install
-	}
+    params = {'cmd', build_type = {"debug", "release"}},
+    condition = function() return path:new('conanfile.txt'):exists() end,
+    tasks = {install = conan_install}
 }
 
 return conan
