ca1cdbf8c5d9f0471706e8b8f2dbded765dec8b0
dreadster3 <afonso.antunes@live.com.pt>
2023-07-01 02:13:27 +0100

    Remove source line

--- lua/tasks/module/cmake.lua
+++ lua/tasks/module/cmake.lua
@@ -148,8 +148,6 @@ local function configure(module_config, _)
 
     local conan_toolchain = build_dir:joinpath('conan_toolchain.cmake')
 
-    io.popen("source " .. build_dir:joinpath("conanbuild.sh").filename)
-
     local args = {
         '-B', build_dir.filename, '-D',
         'CMAKE_TOOLCHAIN_FILE=' .. conan_toolchain.filename, '-D',
