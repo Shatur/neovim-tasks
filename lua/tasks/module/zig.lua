9639d275a6799d99b7420ff7e7cc61288b486fc1
Nenad Mikša <nenad.miksa@microblink.com>
2025-05-03 19:40:19 +0200

    chore: apply lua formatting

--- lua/tasks/module/zig.lua
+++ lua/tasks/module/zig.lua
@@ -1,4 +1,4 @@
-local Path = require( 'plenary.path' )
+local Path = require('plenary.path')
 
 local build_type_map = {
     Debug = 'off',
@@ -7,73 +7,73 @@ local build_type_map = {
     ReleaseFast = 'fast',
 }
 
-local function build( module_config, _ )
+local function build(module_config, _)
     local build_target = module_config.build_step
     local build_type = build_type_map[module_config.build_type]
     return {
         cmd = module_config.cmd,
         args = { 'build', build_target, '--release=' .. build_type }
-      }
+    }
 end
 
 local function get_build_steps()
-    local allSteps = vim.fn.systemlist( 'zig build --list-steps' )
+    local allSteps = vim.fn.systemlist('zig build --list-steps')
     local stepNames = {}
-    for _, step in ipairs( allSteps ) do
-        local firstWord = string.gmatch( step, "%w+" )()
-        table.insert( stepNames, firstWord )
+    for _, step in ipairs(allSteps) do
+        local firstWord = string.gmatch(step, "%w+")()
+        table.insert(stepNames, firstWord)
     end
     return stepNames
 end
 
-local function clean( _, _ )
+local function clean(_, _)
     -- note: requires unix shell
-     return {
+    return {
         cmd = 'rm',
         args = { '-rf', 'zig-out' },
-     }
+    }
 end
 
-local function clean_cache( _, _ )
+local function clean_cache(_, _)
     -- note: requires unix shell
-     return {
+    return {
         cmd = 'rm',
         args = { '-rf', '.zig-cache' },
-     }
+    }
 end
 
-local function run_file( module_config, _ )
-    local currentSource = vim.fn.expand( '%' )
-     return {
+local function run_file(module_config, _)
+    local currentSource = vim.fn.expand('%')
+    return {
         cmd = module_config.cmd,
         args = { 'run', '-O' .. module_config.build_type, currentSource, '--' }
-      }
+    }
 end
 
-local function build_file_as_exe( module_config, _ )
+local function build_file_as_exe(module_config, _)
     local currentSource = vim.fn.expand('%')
     local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
-     return {
+    return {
         cmd = module_config.cmd,
         args = { 'build-exe', currentSource, '-femit-bin=.zig-cache/run-' .. srcFilename, '-O' .. module_config.build_type },
-      }
+    }
 end
 
-local function debug_file( module_config, _ )
+local function debug_file(module_config, _)
     local currentSource = vim.fn.expand('%')
     local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
-     return {
+    return {
         cmd = '.zig-cache/run-' .. srcFilename,
         dap_name = module_config.dap_name,
-      }
+    }
 end
 
-local function test_file( module_config, _ )
-    local currentSource = vim.fn.expand( '%' )
-     return {
+local function test_file(module_config, _)
+    local currentSource = vim.fn.expand('%')
+    return {
         cmd = module_config.cmd,
         args = { 'test', '-O' .. module_config.build_type, currentSource },
-      }
+    }
 end
 
 local function build_test_file(module_config, _)
@@ -95,18 +95,18 @@ local function debug_test_file(module_config, _)
 end
 
 local function get_current_test_filter()
-    local test_start_line = vim.fn.search( "^test", 'bWn' );
+    local test_start_line = vim.fn.search("^test", 'bWn');
     if test_start_line ~= 0 then
-        local line_contents = vim.fn.getline( test_start_line )
-        local test_filter = string.gsub( line_contents, "^test +\"(.+)\".+$", "%1" )
+        local line_contents = vim.fn.getline(test_start_line)
+        local test_filter = string.gsub(line_contents, "^test +\"(.+)\".+$", "%1")
         return test_filter
     else
         return nil
     end
 end
 
-local function run_current_test( module_config, _ )
-    local currentSource = vim.fn.expand( '%' )
+local function run_current_test(module_config, _)
+    local currentSource = vim.fn.expand('%')
     local test_filter = get_current_test_filter()
     if test_filter ~= nil then
         return {
@@ -135,7 +135,6 @@ local function build_current_test(module_config, _)
             cmd = module_config.cmd,
             args = { 'test', currentSource, '-femit-bin=.zig-cache/test-' .. srcFilename, '--test-no-exec', '-O' .. module_config.build_type, "--test-filter", test_filter },
         }
-
     end
 end
 
@@ -146,7 +145,7 @@ return {
         build_type = { 'Debug', 'ReleaseSafe', 'ReleaseFast', 'ReleaseSmall' },
         build_step = get_build_steps,
     },
-    condition = function() return Path:new( 'zig.build' ):exists() end,
+    condition = function() return Path:new('zig.build'):exists() end,
     tasks = {
         build = build,
         clean = clean,
