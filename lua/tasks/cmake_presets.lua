ea56912646973fc37d0bbab30d9b960f3fdfae2c
Nenad Mikša <nenad.miksa@microblink.com>
2025-05-04 11:03:43 +0200

    feat: added cmake-kits from DoDoENT/neovim-additional-tasks

---
+++ lua/tasks/cmake_presets.lua
@@ -0,0 +1,250 @@
+-- shamelessly stolen from https://github.com/Civitasv/cmake-tools.nvim/blob/c0cb2c98ed03800a1b1f7fc29d4b783aec96f530/lua/cmake-tools/presets.lua
+
+local Path = require("plenary.path")
+
+local presets = {}
+
+-- Checks if there is a CMakePresets.json or CMakeUserPresets.json file
+-- in the given directory, a CMakeUserPresets.json is
+-- preferred over CMakePresets.json as CMakePresets.json
+-- is implicitly included by CMakeUserPresets.json
+function presets.check( path )
+    path = path or "."
+    -- helper function to find the config file
+    -- returns file path if found, nil otherwise
+    local function findcfg(dirPath)
+        local files = vim.fn.readdir(dirPath)
+        local file = nil
+        local presetFiles = {}
+        for _, f in ipairs(files) do                                         -- iterate over files in current directory
+            if f == "CMakePresets.json" or f == "CMakeUserPresets.json" then -- if a preset file is found
+                presetFiles[#presetFiles + 1] = vim.fn.resolve(dirPath .. "/" .. f)
+            end
+        end
+        table.sort(presetFiles, function(a, b)
+            return a < b
+        end)
+        if #presetFiles > 0 then
+            file = presetFiles[#presetFiles]
+        end
+        return file
+    end
+
+    local file = findcfg( path ) -- check for config file
+
+    return file
+end
+
+-- Extends (or creates a new) key-value pair in [dest] in which the
+-- key is [key] and the value is the resulting list table of merging
+-- dst[key] and src[key].
+-- This function mutates dest.
+local function merge_table_list_by_key(dst, src, key)
+    if not dst[key] then
+        dst[key] = {}
+    end
+    vim.list_extend(dst[key], src[key])
+end
+
+-- Decodes a Cmake[User]Presets.json and its "includes", if any
+-- CMakeUserPresets.json implicitly includes CMakePresets.json if it exists
+local function decode(file)
+    local data = vim.fn.json_decode(vim.fn.readfile(file))
+    if not data then
+        error(string.format("Could not parse %s", file))
+    end
+    local includes = data["include"]
+    local isUserPreset = string.find(file, "CMakeUserPresets.json")
+    if not includes and isUserPreset then
+        local parentDir = vim.fs.dirname(file)
+        local parentPreset = parentDir .. "/CMakePresets.json"
+        if vim.fn.filereadable(parentPreset) then
+            includes = {
+                parentPreset,
+            }
+        end
+    end
+    if not includes then
+        return data
+    end
+
+    for _, f in ipairs(includes) do
+        local fpath = Path:new( f )
+        if not fpath:is_absolute() then
+            fpath = Path:new( file ):parent():joinpath( fpath )
+            f = tostring(fpath)
+        end
+        if Path:new( f ):exists() then
+            local fdata = vim.fn.json_decode(vim.fn.readfile(f))
+            local thisFilePresetKeys = vim.tbl_filter(function(key)
+                return string.find(key, "Presets")
+            end, vim.tbl_keys(fdata))
+
+            for _, eachPreset in ipairs(thisFilePresetKeys) do
+                merge_table_list_by_key(data, fdata, eachPreset)
+            end
+        else
+            error(string.format("Invalid include path %s", f ) )
+        end
+    end
+
+    return data
+end
+
+-- Retrieve all presets with type
+-- @param type: `buildPresets` or `configurePresets`
+-- @param {opts}: include_hidden(bool|nil).
+--                If true, hidden preset will be included in result.
+-- @param path: Path to directory where presets file will be loaded from
+-- @returns : list with all preset names
+function presets.parse(type, opts, path)
+    local file = presets.check(path)
+    local options = {}
+    if not file then
+        return options
+    end
+    local include_hidden = opts and opts.include_hidden
+    local data = decode(file)
+    if not data then
+        error("Error when parsing the presets file")
+    end
+    for _, v in pairs(data[type]) do
+        if include_hidden or not v["hidden"] then
+            table.insert(options, v["name"])
+        end
+    end
+    return options
+end
+
+-- Retrieve all presets with type
+-- @param type: `buildPresets` or `configurePresets`
+-- @param {opts}: include_hidden(bool|nil).
+--                If true, hidden preset will be included in result.
+-- @param path: Path to directory where presets file will be loaded from
+-- @returns : table with preset name as key and preset content as value
+function presets.parse_name_mapped(type, opts, path)
+    local file = presets.check(path)
+    local options = {}
+    if not file then
+        return options
+    end
+    local include_hidden = opts and opts.include_hidden
+    local data = decode(file)
+    if not data then
+        error("Error when parsing the presets file")
+    end
+    for _, v in pairs(data[type]) do
+        if include_hidden or not v["hidden"] then
+            options[v["name"]] = v
+        end
+    end
+    return options
+end
+
+-- Retrieve preset by name and type
+-- @param name: from `name` option
+-- @param type: `buildPresets` or `configurePresets`
+-- @param path: Path to directory where presets file will be loaded from
+function presets.get_preset_by_name(name, type, path)
+    local file = presets.check(path)
+    if not file then
+        return nil
+    end
+    local data = decode(file)
+    if data[ type ] then
+        for _, v in pairs(data[type]) do
+            if v.name == name then
+                return v
+            end
+        end
+    end
+    return nil
+end
+
+-- Retrieve build type from preset
+function presets.get_build_type(preset)
+    if preset and preset.cacheVariables and preset.cacheVariables.CMAKE_BUILD_TYPE then
+        return preset.cacheVariables.CMAKE_BUILD_TYPE
+    end
+    return "Debug"
+end
+
+-- Retrieve build directory from preset
+function presets.get_build_dir(preset)
+    if not preset then
+        return nil
+    end
+    -- check if this preset is extended
+    local function helper(p_preset)
+        local build_dir = ""
+
+        if not p_preset then
+            return build_dir
+        end
+
+        if p_preset.inherits then
+            local inherits = p_preset.inherits
+            local set_dir_by_parent = function(parent)
+                local ppreset = presets.get_preset_by_name(parent, "configurePresets")
+                local ppreset_build_dir = helper(ppreset)
+                if ppreset_build_dir ~= "" then
+                    build_dir = ppreset_build_dir
+                end
+            end
+
+            -- According to `https://cmake.org/cmake/help/latest/manual/cmake-presets.7.html`,
+            -- `inherits` field may be a list of strings or a single string.
+            -- Check type then act.
+            if type(inherits) == "table" then
+                -- iterate inherits from end, cause
+                -- If multiple inherits presets provide conflicting
+                -- values for the same field, the earlier preset in
+                -- the inherits array will be preferred.
+                for i = #inherits, 1, -1 do
+                    local parent = inherits[i]
+
+                    -- retrieve its parent preset
+                    set_dir_by_parent(parent)
+                end
+            elseif type(inherits) == "string" then
+                set_dir_by_parent(inherits)
+            end
+        end
+
+        if p_preset.binaryDir then
+            build_dir = p_preset.binaryDir
+        end
+
+        return build_dir
+    end
+
+    local build_dir = helper(preset)
+
+    -- macro expansion
+    local source_path = Path:new(vim.loop.cwd())
+    local source_relative = vim.fn.fnamemodify(vim.loop.cwd(), ":t")
+
+    local cwd = vim.loop.cwd()
+    if not cwd then
+        cwd = "."
+    end
+    build_dir = build_dir:gsub("${sourceDir}", cwd)
+    build_dir = build_dir:gsub("${sourceParentDir}", source_path:parent().filename)
+    build_dir = build_dir:gsub("${sourceDirName}", source_relative)
+    build_dir = build_dir:gsub("${presetName}", preset.name)
+    if preset.generator then
+        build_dir = build_dir:gsub("${generator}", preset.generator)
+    end
+    build_dir = build_dir:gsub("${hostSystemName}", vim.loop.os_uname().sysname)
+    build_dir = build_dir:gsub("${fileDir}", source_path.filename)
+    build_dir = build_dir:gsub("${dollar}", "$")
+    build_dir = build_dir:gsub("${pathListSep}", "/")
+
+    build_dir = build_dir:gsub("%$env{(.-)}", function(env_var)
+        return vim.fn.getenv(env_var) or ""
+    end)
+
+    return build_dir
+end
+
+return presets
---
+++ lua/tasks/module/cmake_kits.lua
@@ -0,0 +1,436 @@
+local Path = require( 'plenary.path' )
+local utils = require( 'tasks.utils' )
+local cmake_utils = require( 'tasks.cmake_kits_utils' )
+local cmake_presets = require( 'tasks.cmake_presets' )
+
+-- based on https://github.com/Shatur/neovim-tasks/blob/master/lua/tasks/module/cmake.lua
+-- but implemented with support for cmake kits
+
+local function getKeys( tbl )
+    local keys = {}
+    for k, _ in pairs( tbl ) do
+        table.insert( keys, k )
+    end
+    return keys
+end
+
+local function getTargetNames()
+    local build_dir = cmake_utils.getBuildDir()
+    if not build_dir:is_dir() then
+        utils.notify( string.format( 'Build directory "%s" does not exist, you need to run "configure" task first', build_dir ), vim.log.levels.ERROR )
+        return nil
+    end
+
+    local reply_dir = cmake_utils.getReplyDir( build_dir )
+    local codemodel_targets = cmake_utils.getCodemodelTargets( reply_dir )
+    if not codemodel_targets then
+        return nil
+    end
+
+    local targets = {}
+    for _, target in ipairs( codemodel_targets ) do
+        local target_info = cmake_utils.getTargetInfo( target, reply_dir )
+        local target_name = target_info[ 'name' ]
+        if target_name:find( '_autogen' ) == nil then
+            table.insert(targets, target_name)
+        end
+    end
+
+    -- always add 'all' target
+    table.insert( targets, 'all' )
+
+    return targets
+end--
+
+local function makeQueryFiles( build_dir )
+    local query_dir = build_dir / '.cmake' / 'api' / 'v1' / 'query'
+    if not query_dir:mkdir( { parents = true } ) then
+        utils.notify( string.format( 'Unable to create "%s"', query_dir.filename ), vim.log.levels.ERROR )
+        return false
+    end
+
+    local codemodel_file = query_dir / 'codemodel-v2'
+    if not codemodel_file:is_file() then
+        if not codemodel_file:touch() then
+            utils.notify( string.format( 'Unable to create "%s"', codemodel_file.filename ), vim.log.levels.ERROR )
+            return false
+        end
+    end
+    return true
+end
+
+-- inspired by https://github.com/Shatur/neovim-tasks/blob/master/lua/tasks/module/cmake.lua#L130
+-- but modified to also support build kits
+local function configure( module_config, _ )
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    if usePresets and not module_config.configure_preset then
+        utils.notify( 'No selected configure preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    local build_dir = cmake_utils.getBuildDir()
+    build_dir:mkdir({ parents = true })
+
+    if not makeQueryFiles( build_dir ) then
+        return nil
+    end
+
+    if usePresets then
+        local currentPreset = module_config.configure_preset
+
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = { '--preset', currentPreset, '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON' },
+            after_success = cmake_utils.reconfigureClangd,
+        }
+    else
+        local buildTypes = cmake_utils.getCMakeBuildTypesFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+        local build_type_config = buildTypes[ module_config.build_type ]
+        local build_kit_config  = cmakeKits[ module_config.build_kit ]
+
+        local cmakeBuildType = build_type_config.build_type
+
+        local generator = build_kit_config.generator and build_kit_config.generator or "Ninja"
+        local buildTypeAware = true
+        if build_kit_config.build_type_aware ~= nil then
+            buildTypeAware = build_kit_config.build_type_aware
+        end
+
+        local args = { '-G', generator, '-B', build_dir.filename, '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON' }
+        if buildTypeAware then
+            table.insert( args, '-DCMAKE_BUILD_TYPE=' .. cmakeBuildType )
+        end
+        if module_config.source_dir then
+            table.insert( args, '-S' )
+            table.insert( args, module_config.source_dir )
+        end
+
+        if build_kit_config.toolchain_file then
+            table.insert( args, '-DCMAKE_TOOLCHAIN_FILE=' .. build_kit_config.toolchain_file )
+        end
+
+        if build_kit_config.compilers then
+            table.insert( args, '-DCMAKE_C_COMPILER=' .. build_kit_config.compilers.C )
+            table.insert( args, '-DCMAKE_CXX_COMPILER=' .. build_kit_config.compilers.CXX )
+        end
+
+        if build_type_config.cmake_usr_args then
+            for k, v in pairs( build_type_config.cmake_usr_args ) do
+                table.insert( args, '-D' .. k .. '=' .. v )
+            end
+        end
+
+        if build_kit_config.cmake_usr_args then
+            for k, v in pairs( build_kit_config.cmake_usr_args ) do
+                table.insert( args, '-D' .. k .. '=' .. v )
+            end
+        end
+
+        return {
+            cmd = module_config.cmd,
+            args = args,
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+            after_success = cmake_utils.reconfigureClangd,
+        }
+    end
+end
+
+local function build( module_config, _ )
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    if usePresets and not module_config.build_preset then
+        utils.notify( 'No selected build preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    if usePresets then
+        local buildPreset = module_config.build_preset
+
+        local args = { '--build', '--preset', buildPreset }
+
+        if module_config.target and module_config.target ~= 'all' then
+            vim.list_extend(args, { '--target', module_config.target })
+        end
+
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = args
+        }
+    else
+        local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+
+        local args = { '--build', build_dir.filename }
+        if module_config.target and module_config.target ~= 'all' then
+            vim.list_extend(args, { '--target', module_config.target })
+        end
+
+        return {
+            cmd = module_config.cmd,
+            args = args,
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+        }
+    end
+end
+
+local function build_all( module_config, _ )
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    if usePresets and not module_config.build_preset then
+        utils.notify( 'No selected build preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    if usePresets then
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = { '--build', '--preset', module_config.build_preset },
+        }
+    else
+        local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+
+        return {
+            cmd = module_config.cmd,
+            args = { '--build', build_dir.filename },
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+        }
+    end
+end
+
+local function build_current_file( module_config, _ )
+
+    local sourceName = vim.fn.expand( '%' )
+    local extension  = vim.fn.fnamemodify( sourceName, ':e' )
+
+    local headerExtensions = {
+        [ 'h' ] = true,
+        [ 'hxx' ] = true,
+        [ 'hpp' ] = true,
+    }
+
+    if #extension == 0 or headerExtensions[ extension ] then
+        vim.notify( 'Given file is not a source file!', vim.log.levels.ERROR, { title = 'cmake_kits' } )
+        return nil
+    end
+
+    local ninjaTarget = vim.fn.fnameescape( vim.fn.fnamemodify( sourceName, ':p' ) .. '^' )
+
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    if usePresets and not module_config.build_preset then
+        utils.notify( 'No selected build preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    if usePresets then
+        local currentPreset = cmake_presets.get_preset_by_name( module_config.configure_preset, 'configurePresets', module_config.source_dir )
+
+        if not currentPreset or ( currentPreset.generator ~= "Ninja" and currentPreset.generator ~= 'Ninja Multi-Config' ) then
+            vim.notify( 'Build current file is supported only for Ninja generator at the moment!', vim.log.levels.ERROR, { title = 'cmake_kits' } )
+            return nil
+        end
+
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = { '--build', '--preset', module_config.build_preset, '--target', ninjaTarget },
+        }
+    else
+        local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+        local build_kit_config  = cmakeKits[ module_config.build_kit ]
+        local generator = build_kit_config.generator and build_kit_config.generator or "Ninja"
+
+        if generator ~= "Ninja" and generator ~= "Ninja Multi-Config" then
+            vim.notify( 'Build current file is supported only for Ninja generator at the moment!', vim.log.levels.ERROR, { title = 'cmake_kits' } )
+            return nil
+        end
+
+        return {
+            cmd = module_config.cmd,
+            args = { '--build', build_dir.filename, '--target', ninjaTarget },
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+        }
+    end
+end
+
+local function clean( module_config, _ )
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    if usePresets and not module_config.build_preset then
+        utils.notify( 'No selected build preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    if usePresets then
+        local args = { '--build', '--preset', module_config.build_preset, '--target', 'clean' }
+
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = args,
+        }
+    else
+        local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+
+        return {
+            cmd = module_config.cmd,
+            cwd = module_config.source_dir,
+            args = { '--build', build_dir.filename, '--target', 'clean' },
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+        }
+    end
+end
+
+local function purgeBuildDir( module_config, _ )
+    local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+
+    return {
+        -- TODO: what about Windows?
+        cmd = 'rm',
+        args = { '-rf', tostring( build_dir ) },
+    }
+end
+
+local function trim( s )
+   return s:match'^%s*(.*%S)' or ''
+end
+
+local function runCTest( module_config, _ )
+    local usePresets = cmake_utils.shouldUsePresets( module_config )
+
+    -- assume test preset names are the same as build preset names
+    -- TODO: add support for separate test presets if needed
+    if usePresets and not module_config.build_preset then
+        utils.notify( 'No selected build preset, please select it', vim.log.levels.ERROR )
+        return nil
+    end
+
+    local numcpus = trim( vim.fn.system( 'nproc' ) )
+    if usePresets then
+        return {
+            cmd = 'ctest',
+            cwd = module_config.source_dir,
+            args = { '--preset', module_config.build_preset, '-j', numcpus, '--output-on-failure' },
+        }
+    else
+        local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+        local cmakeKits = cmake_utils.getCMakeKitsFromConfig( module_config )
+
+        return {
+            cmd = 'ctest',
+            args = { '-C', module_config.build_type, '-j', numcpus, '--output-on-failure' },
+            cwd = tostring( build_dir ),
+            env = cmakeKits[ module_config.build_kit ].environment_variables,
+        }
+    end
+end
+
+
+local function run( module_config, _ )
+    if not module_config.target then
+        utils.notify( 'No selected target, please set "target" parameter', vim.log.levels.ERROR )
+        return nil
+    end
+
+    local build_dir = cmake_utils.getBuildDirFromConfig( module_config )
+    if not build_dir:is_dir() then
+        utils.notify( string.format( 'Build directory "%s" does not exist, you need to run "configure" task first', build_dir ), vim.log.levels.ERROR )
+        return nil
+    end
+
+    local target_path = cmake_utils.getExecutablePath(
+        build_dir,
+        module_config.target,
+        cmake_utils.getCurrentBuildType( module_config ),
+        cmake_utils.getReplyDir( build_dir )
+    )
+    if not target_path then
+        return
+    end
+
+    if not target_path:is_file() then
+        utils.notify( string.format( 'Selected target "%s" is not built', target_path.filename ), vim.log.levels.ERROR )
+        return nil
+    end
+
+    return {
+        cmd = target_path.filename,
+        cwd = target_path:parent().filename,
+    }
+end
+
+local function debug( module_config, _ )
+    local command = run( module_config, nil )
+    if not command then
+        return nil
+    end
+
+    command.dap_name = module_config.dap_name
+    return command
+end
+
+local function setupCMakeDAP( configure_command )
+    local dap = require('dap')
+    local args = { '--debugger', '--debugger-pipe=${pipe}' }
+    vim.list_extend( args, configure_command.args )
+
+    dap.adapters.cmake = {
+        type = 'pipe',
+        pipe = '${pipe}',
+        executable = {
+            command = configure_command.cmd,
+            args = args,
+        },
+    }
+    dap.configurations.cmake = {
+        type = 'cmake',
+        request = 'attach',
+        name = 'CMake Debugger',
+    }
+end
+
+local function configureDebug( module_config, _ )
+    local command = configure( module_config, nil )
+    if not command then
+        return nil
+    end
+
+    setupCMakeDAP( command )
+
+    command.dap_name = 'cmake'
+    return command
+end
+
+return {
+    params = {
+        target           = getTargetNames,
+        build_type       = function() return getKeys( cmake_utils.getCMakeBuildTypes() ) end,
+        build_kit        = function() return getKeys( cmake_utils.getCMakeKits() ) end,
+        configure_preset = function() return cmake_presets.parse( 'configurePresets' ) end,
+        build_preset     = function() return cmake_presets.parse( 'buildPresets'     ) end,
+        ignore_presets   = {true, false},
+    },
+    condition = function() return Path:new( 'CMakeLists.txt' ):exists() end,
+    tasks = {
+        configure = configure,
+        configureDebug = configureDebug,
+        build = build,
+        build_all = build_all,
+        build_current_file = build_current_file,
+        run = { build, run },
+        debug = { build, debug },
+        clean = clean,
+        rebuild = { clean, build },
+        ctest = runCTest,
+        purge = purgeBuildDir,
+        reconfigure = { purgeBuildDir, configure }
+    }
+}
