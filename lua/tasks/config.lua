4011b5c1ae94ff518512e5bf6014499072fcdb59
Nenad Mikša <nenad.miksa@microblink.com>
2025-05-04 12:20:41 +0200

    docs: updated README to reflect information about new cmake tasks

--- lua/tasks/config.lua
+++ lua/tasks/config.lua
@@ -8,7 +8,7 @@ local config = {
       build_type = 'Debug', -- Build type, can be changed using `:Task set_module_param cmake build_type`.
       build_kit = 'default',  -- default build kit, can be changed using `:Task set_module_param cmake build_kit`.
       dap_name = 'codelldb', -- DAP configuration name from `require('dap').configurations`. If there is no such configuration, a new one with this name as `type` will be created.
-      build_dir = tostring(Path:new('{cwd}', 'build', '{build_kit}-{build_type}')), -- Build directory. The expressions `{cwd}`, `{build_kit}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
+      build_dir = tostring(Path:new('{cwd}', 'build', '{build_kit}', '{build_type}')), -- Build directory. The expressions `{cwd}`, `{build_kit}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
       cmake_kits_file = nil, -- set path to JSON file containing cmake kits
       cmake_build_types_file = nil, -- set path to JSON file containing cmake kits
       clangd_cmdline = { 'clangd', '--background-index', '--clang-tidy', '--header-insertion=never', '--completion-style=detailed', '--offset-encoding=utf-8', '--pch-storage=memory', '--cross-file-rename', '-j=4' }, -- command line for invoking clangd - this array will be extended with --compile-commands-dir and --query-driver after each cmake configure with parameters inferred from build_kit, build_type and build_dir
